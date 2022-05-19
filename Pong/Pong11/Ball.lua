89b2549d7202e8e3206cca11145067c1939424ff
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-05-19 19:41:14 -0400

    Initial commit

---
+++ Pong/Pong11/Ball.lua
@@ -0,0 +1,43 @@
+Ball = Class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x 
+    self.y = y 
+    self.width = width 
+    self.height = height 
+
+    
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50) 
+end
+
+function Ball:collides(box)
+    if self.x > box.x + box.width or self.x + self.width < box.x then 
+        return false
+    end 
+
+    if self.y > box.y + box.height or self.y + self.height < box.y then 
+        return false
+    end
+
+    return true
+end
+function Ball:reset()
+
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50) * 1.5
+end
+
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+
+    love.graphics.rectangle('fill', self.x, self.y, 5, 5)
+
+end
---
+++ README.md
@@ -0,0 +1,1 @@
+# ICS3U-SinglePlayerPong-LUA
