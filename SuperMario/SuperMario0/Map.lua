164f89e49488d3306acf8ba3635348b80ddd7104
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-05-22 14:18:27 -0400

    FinalCommit

---
+++ SuperMario/SuperMario0/Map.lua
@@ -0,0 +1,13 @@
+Map = Class{}
+
+function Map:init()
+
+end
+
+function Map:update(dt)
+
+end
+
+function Map:render()
+
+end
---
+++ SuperMario/SuperMario6/Player.lua
@@ -0,0 +1,108 @@
+
+
+Player = Class{}
+
+local WALKING_SPEED = 100
+
+function Player:init(map)
+    
+    self.x = 0
+    self.y = 0
+    self.width = 16
+    self.height = 20
+
+    self.xOffset = 8
+    self.yOffset = 16
+
+    self.map = map
+    self.texture = love.graphics.newImage('graphics/blue_alien.png')
+
+    self.frames = {}
+
+    self.currentFrame = nil
+
+    self.state = 'idle'
+
+    self.direction = 'left'
+
+    self.dx = 0
+    self.dy = 0
+
+    self.y = map.tileHeight * ((map.mapHeight - 2) / 2) - self.height
+    self.x = map.tileWidth * 10
+
+    self.animations = {
+        ['idle'] = Animation({
+            texture = self.texture,
+            frames = {
+                love.graphics.newQuad(0, 0, 16, 20, self.texture:getDimensions())
+            }
+        }),
+        ['walking'] = Animation({
+            texture = self.texture,
+            frames = {
+                love.graphics.newQuad(128, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(160, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
+            },
+            interval = 0.15
+        })
+    }
+
+    self.animation = self.animations['idle']
+    self.currentFrame = self.animation:getCurrentFrame()
+
+    self.behaviors = {
+        ['idle'] = function(dt)
+            
+            if love.keyboard.isDown('left') then
+                self.direction = 'left'
+                self.dx = -WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
+                self.animation = self.animations['walking']
+            elseif love.keyboard.isDown('right') then
+                self.direction = 'right'
+                self.dx = WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
+                self.animation = self.animations['walking']
+            end
+        end,
+        ['walking'] = function(dt)
+            
+            if love.keyboard.isDown('left') then
+                self.direction = 'left'
+                self.dx = -WALKING_SPEED
+            elseif love.keyboard.isDown('right') then
+                self.direction = 'right'
+                self.dx = WALKING_SPEED
+            else
+                self.dx = 0
+                self.state = 'idle'
+                self.animation = self.animations['idle']
+            end
+        end
+    }
+end
+
+function Player:update(dt)
+    self.behaviors[self.state](dt)
+    self.animation:update(dt)
+    self.currentFrame = self.animation:getCurrentFrame()
+    self.x = self.x + self.dx * dt
+end
+
+function Player:render()
+    local scaleX
+
+    if self.direction == 'right' then
+        scaleX = 1
+    else
+        scaleX = -1
+    end
+
+    love.graphics.draw(self.texture, self.currentFrame, math.floor(self.x + self.xOffset),
+        math.floor(self.y + self.yOffset), 0, scaleX, 1, self.xOffset, self.yOffset)
+end
