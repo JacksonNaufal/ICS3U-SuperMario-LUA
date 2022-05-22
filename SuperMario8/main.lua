77df258153a84bf16e8ea3114bf025758846e88b
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-05-22 14:15:45 -0400

    Commiting

---
+++ SuperMario8/main.lua
@@ -0,0 +1,82 @@
+
+Class = require 'class'
+push = require 'push'
+
+require 'Animation'
+require 'Map'
+require 'Player'
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+math.randomseed(os.time())
+
+map = Map()
+
+function love.load()
+    
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.graphics.setFont(love.graphics.newFont('fonts/font.ttf', 8))
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true
+    })
+
+    love.keyboard.keysPressed = {}
+    love.keyboard.keysReleased = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keyboard.wasPressed(key)
+    if (love.keyboard.keysPressed[key]) then
+        return true
+    else
+        return false
+    end
+end
+
+function love.keyboard.wasReleased(key)
+    if (love.keyboard.keysReleased[key]) then
+        return true
+    else
+        return false
+    end
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyreleased(key)
+    love.keyboard.keysReleased[key] = true
+end
+
+function love.update(dt)
+    map:update(dt)
+
+    love.keyboard.keysPressed = {}
+    love.keyboard.keysReleased = {}
+end
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(108 / 255, 140 / 255, 255 / 255, 255 / 255)
+
+    love.graphics.translate(math.floor(-map.camX + 0.5), math.floor(-map.camY + 0.5))
+    map:render()
+
+    push:apply('end')
+end
